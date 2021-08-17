cde172abc8eea19ea1c87de8da43522ca42ad012
mehtaReena <j.reena07@gmail.com>
2021-08-17 17:37:28 +0530

    commit

--- pages/NavBar.jsx
+++ pages/NavBar.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 function NavBar(props) {
     return (
         <div className={styles.nav} >
-
+           <h2>Ticket Tracker</h2>
         </div>
     );
 }
